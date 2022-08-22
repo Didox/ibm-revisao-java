d652cf61ea1e6c5273d2a19eebab60782b31b784
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-08-22 11:29:32 -0300

    iniciando projeto com spring no console

---
+++ REAME.md
@@ -0,0 +1,8 @@
+  
+  ```shell
+
+  mvn spring-boot:start # rodando com o maven instalado no comoutador
+  ./mvnw spring-boot:start # roda com unix e o binário do maven no pasta oculta .mvn
+  mvnw.cmd spring-boot:start # roda com MSDos(windows) e o binário do maven no pasta oculta .mvn
+
+  ```
---
+++ src/main/java/com/br/danilo/console/LogicaApplication.java
@@ -0,0 +1,16 @@
+package com.br.danilo.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicaApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Bem vindo ao app java console");
+
+		//SpringApplication.run(LogicaApplication.class, args);
+	}
+
+}
