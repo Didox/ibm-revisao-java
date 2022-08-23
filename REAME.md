cc4c008911491da20e83cf2dee07005b8d658486
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-08-23 10:57:38 -0300

    iniciando logica para explicar POO

--- REAME.md
+++ REAME.md
@@ -1,8 +1,35 @@
-  
-  ```shell
 
-  mvn spring-boot:start # rodando com o maven instalado no comoutador
-  ./mvnw spring-boot:start # roda com unix e o binário do maven no pasta oculta .mvn
-  mvnw.cmd spring-boot:start # roda com MSDos(windows) e o binário do maven no pasta oculta .mvn
+```shell
 
-  ```
+mvn spring-boot:start # rodando com o maven instalado no comoutador
+./mvnw spring-boot:start # roda com unix e o binário do maven no pasta oculta .mvn
+mvnw.cmd spring-boot:start # roda com MSDos(windows) e o binário do maven no pasta oculta .mvn
+
+```
+
+## Exercicio 1
+```shell
+Dado que eu tenha uma estrutura feita em java
+faça um programa para capturar o nome do usuário
+e mostrar este nome na tela.
+
+Obs: Capriche nos detalhes, crie uma boa usabilidade
+```
+
+## Exercicio 2
+```shell
+João é um comerciante e precisa automatizar o seu processo de vendas
+Para automatizar este processo João chamou a empresa SUA_EMPRESA(Você aluno)
+O problema de joão é que ele precisa cadastrar os seus produtos
+e vender estes produtos para seus clientes.
+
+Baseado nesta demanda, faça um programa que ajude o João a vender mais.
+
+Este programa precisa ter uma entrada e só sair do programa, quando o João
+quiser.
+
+E ter opcão de:
+- Cadastrar produtos
+- Vender produtos (Criar pedidos, com clientes)
+- Mostrar relatório dos mesmos
+```
--- src/main/java/com/br/danilo/console/LogicaApplication.java
+++ src/main/java/com/br/danilo/console/LogicaApplication.java
@@ -1,14 +1,105 @@
 package com.br.danilo.console;
 
-import org.springframework.boot.SpringApplication;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LogicaApplication {
+	
+	public final static void clearConsole()
+	{
+		System.out.print("\033[H\033[2J");  
+		System.out.flush(); 
+	}
 
-	public static void main(String[] args) {
+	public final static void espera(int segundos) throws InterruptedException
+	{
+		TimeUnit.SECONDS.sleep(segundos);
+	}
 
-		System.out.println("Bem vindo ao app java console");
+    
+	private static List<String> produtos = new ArrayList<>();
+	private static List<List<String>> pedidos = new ArrayList<>();
+	// private static List<String> clientes = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		System.out.println("==== [ Olá João segue o program para resolver os seus problemas :) ] =====");
+
+		BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+
+		while(true){
+
+			System.out.println("=== O que você deseja fazer ?  ===");
+			System.out.println("1 - Cadastrar produtos");
+			System.out.println("2 - Cadastrar pedidos");
+			System.out.println("3 - Mostrar relatório");
+			System.out.println("4 - Sair");
+
+			int opcao = Integer.parseInt(reader.readLine());
+
+			clearConsole();
+
+			boolean sair = false;
+			switch(opcao){
+				case 1:
+					System.out.println("Digite o nome do produto");
+					String nome = reader.readLine();
+					produtos.add(nome);
+					clearConsole();
+					System.out.println("Produto cadastrado com sucesso");
+					espera(2);
+					clearConsole();
+					break;
+				case 2:
+					System.out.println("Selecione um produto da lista");
+					for (int i = 0; i< produtos.size(); i++ ) {
+						System.out.println((i+1) + " - " + produtos.get(i));
+					}
+					int idProduto = Integer.parseInt(reader.readLine());
+
+					var clientePedido = new ArrayList<String>();
+					clientePedido.add(produtos.get(idProduto-1));
+
+					System.out.println("Digite o nome do cliente");
+					String nomeCliente = reader.readLine();
+					clientePedido.add(nomeCliente);
+
+					pedidos.add(clientePedido);
+
+					clearConsole();
+					System.out.println("Produto cadastrado com sucesso");
+					espera(2);
+					clearConsole();
+					break;
+				case 3:
+					System.out.println("=== Relatório de pedidos ===");
+					for (List<String> clientePed: pedidos) {
+						System.out.println("Produto: " + clientePed.get(0));
+						System.out.println("Cliente: " + clientePed.get(1));
+					}
+
+					espera(4);
+					clearConsole();
+					break;
+				case 4:
+					sair = true;
+					break;
+			}
+
+			if(sair) break;
+			
+		}
+
+
+ 
+ 
 
 		//SpringApplication.run(LogicaApplication.class, args);
 	}
